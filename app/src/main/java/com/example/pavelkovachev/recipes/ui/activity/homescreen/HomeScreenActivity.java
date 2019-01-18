83f6d32cc0f6ed6c0aa4f450c3596348166ee93b
Pavel Kovachev <pavel.kovachev@adastragrp.com>
2019-01-18 13:21:02 +0200

    Create new fragment_home_screen layout Create new package menu Create search_icon.xml in menu package Add search icon in toolbar

--- app/src/main/java/com/example/pavelkovachev/recipes/ui/activity/homescreen/HomeScreenActivity.java
+++ app/src/main/java/com/example/pavelkovachev/recipes/ui/activity/homescreen/HomeScreenActivity.java
@@ -3,15 +3,31 @@ package com.example.pavelkovachev.recipes.ui.activity.homescreen;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
 
 import com.example.pavelkovachev.recipes.R;
 
 public class HomeScreenActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_home_screen);
+        setContentView(R.layout.activity_homescreen);
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_icon,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
