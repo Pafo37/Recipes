c678202be0438acbdc286eb9140a943ffe21c50d
Pavel Kovachev <pavel.kovachev@adastragrp.com>
2019-01-29 15:33:50 +0200

    Add color white Add toolbar with title

--- app/src/main/java/com/example/pavelkovachev/recipes/ui/activity/tablayout/FavoritesMyRecipesActivity.java
+++ app/src/main/java/com/example/pavelkovachev/recipes/ui/activity/tablayout/FavoritesMyRecipesActivity.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
+
 
 import com.example.pavelkovachev.recipes.R;
 import com.example.pavelkovachev.recipes.ui.fragment.tablayout.FavoritesMyRecipesFragment;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class FavoritesMyRecipesActivity extends AppCompatActivity {
     @BindView(R.id.toolbar_favorites_my_recipes)
@@ -19,6 +21,9 @@ public class FavoritesMyRecipesActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites_my_recipes);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        setTitle("Your Recipes");
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.fragment_container_favorites_my_recipes, FavoritesMyRecipesFragment.newInstance());
         fragmentTransaction.commit();
