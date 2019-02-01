ca26224ccfabe493bca85f6a3bd4fd465b53f241
Pavel Kovachev <pavel.kovachev@adastragrp.com>
2019-02-01 11:10:25 +0200

    fix navigation up

--- app/src/main/java/com/example/pavelkovachev/recipes/ui/activity/generalmealdescription/GeneralMealDescriptionActivity.java
+++ app/src/main/java/com/example/pavelkovachev/recipes/ui/activity/generalmealdescription/GeneralMealDescriptionActivity.java
@@ -2,11 +2,9 @@ package com.example.pavelkovachev.recipes.ui.activity.generalmealdescription;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
 
 import com.example.pavelkovachev.recipes.R;
 import com.example.pavelkovachev.recipes.ui.fragment.generalmealdescription.GeneralMealDescriptionFragment;
@@ -29,5 +27,11 @@ public class GeneralMealDescriptionActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.container_general_meal_description, GeneralMealDescriptionFragment.newInstance());
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
     }
 }
--- app/src/main/java/com/example/pavelkovachev/recipes/ui/fragment/recipeslist/RecipesListFragment.java
+++ app/src/main/java/com/example/pavelkovachev/recipes/ui/fragment/recipeslist/RecipesListFragment.java
@@ -1,5 +1,6 @@
 package com.example.pavelkovachev.recipes.ui.fragment.recipeslist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,14 +10,16 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.example.pavelkovachev.recipes.DataModel;
 import com.example.pavelkovachev.recipes.R;
 import com.example.pavelkovachev.recipes.adapters.recipeslist.RecipesListAdapter;
+import com.example.pavelkovachev.recipes.ui.activity.generalmealdescription.GeneralMealDescriptionActivity;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP;
 
 public class RecipesListFragment extends Fragment implements RecipesListAdapter.ItemListener {
 
@@ -42,8 +45,9 @@ public class RecipesListFragment extends Fragment implements RecipesListAdapter.
 
     @Override
     public void onItemClick(DataModel item) {
-        //this is for testing purposes
-        Toast.makeText(getContext(), item.text + " is clicked", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getActivity(), GeneralMealDescriptionActivity.class);
+        intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 
     @Override
