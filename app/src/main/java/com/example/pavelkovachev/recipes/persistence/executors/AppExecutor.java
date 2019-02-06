98181bfd802f8653c564ca989a5dba7de96b0652
Pavel Kovachev <pavel.kovachev@adastragrp.com>
2019-02-06 16:32:41 +0200

    Made AppExecutor singleton

--- app/src/main/java/com/example/pavelkovachev/recipes/persistence/executors/AppExecutor.java
+++ app/src/main/java/com/example/pavelkovachev/recipes/persistence/executors/AppExecutor.java
@@ -1,12 +1,21 @@
 package com.example.pavelkovachev.recipes.persistence.executors;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class AppExecutor {
 
-    private final Executor executor;
+    private static Executor executor = null;
+    private static final Object LOCK = new Object();
 
-    public AppExecutor(Executor executor) {
-        this.executor = executor;
+    public synchronized static Executor getInstance() {
+        if (executor == null) {
+            synchronized (LOCK) {
+                if (executor == null) {
+                    executor = Executors.newSingleThreadExecutor();
+                }
+            }
+        }
+        return executor;
     }
 }
