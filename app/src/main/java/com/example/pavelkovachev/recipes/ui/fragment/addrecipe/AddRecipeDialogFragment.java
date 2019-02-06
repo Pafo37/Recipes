b7ebf3ddef4ed49b0cc357f41ca61f1c22046c65
Pavel Kovachev <pavel.kovachev@adastragrp.com>
2019-02-06 14:24:37 +0200

    Add Executor class Add methods in Repository

--- app/src/main/java/com/example/pavelkovachev/recipes/ui/fragment/addrecipe/AddRecipeDialogFragment.java
+++ app/src/main/java/com/example/pavelkovachev/recipes/ui/fragment/addrecipe/AddRecipeDialogFragment.java
@@ -22,7 +22,7 @@ import butterknife.OnClick;
 public class AddRecipeDialogFragment extends DialogFragment {
     @BindView(R.id.img_meal_favorites)
     ImageView imgPictureTaken;
-    @BindView(R.id.txt_favorites_description_body)
+    @BindView(R.id.edt_favorites_description_body)
     EditText edtMealDescription;
 
     public static AddRecipeDialogFragment newInstance() {
