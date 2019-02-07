3c13f04876eca225ff54aacb94cf0dec0df6e321
Pavel Kovachev <pavel.kovachev@adastragrp.com>
2019-02-07 12:20:32 +0200

    Create base activity and fragment and refactor the project

---
+++ app/src/main/java/com/example/pavelkovachev/recipes/ui/fragment/base/BaseFragment.java
@@ -0,0 +1,29 @@
+package com.example.pavelkovachev.recipes.ui.fragment.base;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+public abstract class BaseFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(getLayoutResId(),container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this,view);
+    }
+
+    @LayoutRes
+    protected abstract int getLayoutResId();
+}
